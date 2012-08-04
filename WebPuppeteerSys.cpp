0f36cd935d9cde4a086d087865cc164bd7ac9a5b
Mark Karpeles <mark@hell.ne.jp>
2012-08-04 15:50:33 +0900

    adding sys.mtgoxApi() method to call mtgox v2 api

--- WebPuppeteerSys.cpp
+++ WebPuppeteerSys.cpp
@@ -124,6 +124,83 @@ QScriptValue WebPuppeteerSys::signedPost(const QString &url, const QString &post
 	return parent->engine().newVariant((rep->readAll()));
 }
 
+QScriptValue WebPuppeteerSys::mtgoxApi(const QString &path, const QString &post, const QString &api_key, const QString &api_secret) {
+	// compatible with MtGox API
+	static quint64 last_once = 0;
+	quint64 once = QDateTime::currentMSecsSinceEpoch();
+	if (once <= last_once) {
+		once = last_once+1;
+	}
+	last_once = once;
+
+	QByteArray data_path = path.toUtf8(); // for hashing
+	QByteArray data_post;
+	if (post == "") {
+		data_post = (post + QString("nonce=%1").arg(once)).toUtf8();
+	} else {
+		data_post = (post + QString("&nonce=%1").arg(once)).toUtf8();
+	}
+
+	QString url = "https://mtgox.com/api/2/"+path;
+	QNetworkRequest req(url);
+	req.setHeader(QNetworkRequest::ContentTypeHeader, "application/x-www-form-urlencoded");
+	req.setRawHeader("Rest-Key", api_key.toLatin1());
+
+	// quick implementation of SHA512 hmac
+	SHA512_CTX ctx;
+	quint8 sha512[SHA512_DIGEST_LENGTH];
+	QByteArray bin_api_secret(QByteArray::fromBase64(api_secret.toLatin1()));
+
+	if (bin_api_secret.size() > SHA512_BLOCK_LENGTH) {
+		SHA512_Init(&ctx);
+		SHA512_Update(&ctx, (quint8*)bin_api_secret.data(), bin_api_secret.size());
+		SHA512_Final(sha512, &ctx);
+		bin_api_secret = QByteArray((char*)&sha512, SHA512_DIGEST_LENGTH);
+	}
+	if (bin_api_secret.size() < SHA512_BLOCK_LENGTH) {
+		bin_api_secret += QByteArray(bin_api_secret.size() - SHA512_BLOCK_LENGTH, 0);
+	}
+
+	QByteArray o_key_pad = QByteArray(SHA512_BLOCK_LENGTH, 0x5c);
+	QByteArray i_key_pad = QByteArray(SHA512_BLOCK_LENGTH, 0x36);
+
+	for(int i = 0; i < SHA512_BLOCK_LENGTH; i++) {
+		o_key_pad[i] = o_key_pad[i] ^ bin_api_secret[i];
+		i_key_pad[i] = i_key_pad[i] ^ bin_api_secret[i];
+	}
+
+	// first round of HMAC
+	SHA512_Init(&ctx);
+	SHA512_Update(&ctx, (quint8*)i_key_pad.data(), i_key_pad.size());
+	SHA512_Update(&ctx, (quint8*)data_path.data(), data_path.size());
+	SHA512_Update(&ctx, (quint8*)data_post.data(), data_post.size());
+	SHA512_Final(sha512, &ctx);
+	bin_api_secret = QByteArray((char*)&sha512, SHA512_DIGEST_LENGTH);
+	// second round of HMAC
+	SHA512_Init(&ctx);
+	SHA512_Update(&ctx, (quint8*)o_key_pad.data(), o_key_pad.size());
+	SHA512_Update(&ctx, (quint8*)bin_api_secret.data(), bin_api_secret.size());
+	SHA512_Final(sha512, &ctx);
+	bin_api_secret = QByteArray((char*)&sha512, SHA512_DIGEST_LENGTH);
+
+	req.setRawHeader("Rest-Sign", bin_api_secret.toBase64());
+
+	QNetworkReply *rep = net.post(req, data_post);
+	QEventLoop e;
+
+	connect(rep, SIGNAL(finished()), &e, SLOT(quit()));
+	e.exec();
+
+	if (rep->error() != QNetworkReply::NoError) {
+		qDebug("GET error: %s", qPrintable(rep->errorString()));
+		rep->deleteLater();
+		return parent->engine().currentContext()->throwError(QScriptContext::UnknownError, rep->errorString());
+	}
+
+	rep->deleteLater();
+	return parent->engine().newVariant((rep->readAll()));
+}
+
 void WebPuppeteerSys::alert(QString string) {
 	QMessageBox::information(NULL, "WebPuppeteer alert", string);
 }
