1f71ee5e33d073dc006233cedc72218ca3642be3
Mark Karpeles <mark@hell.ne.jp>
2013-02-05 14:43:54 +0900

    poland: use unified mtgox api for in/out

--- bin/poland_withdraw_eur.js
+++ bin/poland_withdraw_eur.js
@@ -5,7 +5,7 @@ sys.include("config.js");
 var b = null;
 
 while(true) {
-	var res = sys.signedPost("https://mtgox.com/api/1/generic/bank/centrum24_pull", "currency=EUR", api_key, api_secret);
+	var res = sys.signedPost("https://mtgox.com/api/1/generic/bank/withdraw_pull", "bank=fe2e074d-e127-42fb-bdd2-9b729029b313&currency=EUR&small_first=1", api_key, api_secret);
 	res = JSON.parse(res);
 	if (res.result != "success") {
 		sys.log(JSON.stringify(res));
@@ -19,7 +19,7 @@ while(true) {
 	sys.log(JSON.stringify(res["return"]));
 	var tx_success = b.processSwift(res["return"]);
 	tx_success.trx = res["return"];
-	sys.signedPost("https://mtgox.com/api/1/generic/bank/centrum24_pull_res", "json="+encodeURIComponent(JSON.stringify(tx_success)), api_key, api_secret);
+	sys.signedPost("https://mtgox.com/api/1/generic/bank/withdraw_pull_res", "bank=fe2e074d-e127-42fb-bdd2-9b729029b313&json="+encodeURIComponent(JSON.stringify(tx_success)), api_key, api_secret);
 }
 
 if (b) b.logout();
