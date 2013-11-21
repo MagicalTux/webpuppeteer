7cc035aa0b7b8834d8a2927eb53f208988d22bbe
Mark Karpeles <mark@hell.ne.jp>
2013-11-22 00:56:01 +0900

    adding script to scan yesterday

---
+++ bin/jpnet_domestic_deposits_yesterday.js
@@ -0,0 +1,16 @@
+// JapanNet Bank - BA Plus
+sys.include("lib/jpnet.js");
+sys.include("config.js");
+
+var bank = new jpnet();
+bank.login(jpnet_account, jpnet_login, jpnet_password);
+
+var data = bank.getYesterdayDeposits();
+
+if (data) {
+	var res = sys.signedPost("https://mtgox.com/api/1/generic/bank/push_csv", "bank=38e619ac-7b9a-49a8-8e70-3628c980d1f0&charset=SJIS&encoding=base64&data="+encodeURIComponent(data), api_key, api_secret);
+	sys.log("JapanNet: Data pushed to server, result: "+res);
+}
+
+bank.logout();
+
